0a297e4f74a24ee0120afc99549625e21a14144c
Kanak <kanakkangdan22@gmail.com>
2023-01-19 21:12:19 +0530

    port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 const userRoute = require('./routes/User');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(helmet());
